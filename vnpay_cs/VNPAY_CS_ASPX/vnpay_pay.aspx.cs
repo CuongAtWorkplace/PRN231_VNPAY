e51b5a5cae90573c1fb55ff5dbe696273692efc7
Cuongcute <105336611+Cuongcute@users.noreply.github.com>
2023-07-10 14:55:33 +0700

    update vnpay

---
+++ vnpay_cs/VNPAY_CS_ASPX/vnpay_pay.aspx.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Configuration;
+using System.Web;
+using log4net;
+using VNPAY_CS_ASPX.Models;
+
+
+namespace VNPAY_CS_ASPX
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        private static readonly ILog log =
+          LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        string amount;
+        string adId;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            amount = Request.QueryString["amount"];
+            adId = Request.QueryString["adId"];
+            lblAmount.Text = amount;
+            HttpCookie cookie = new HttpCookie("adId", adId);
+            cookie.Expires = DateTime.Now.AddDays(1);
+            Response.Cookies.Add(cookie);
+
+
+        }
+
+        protected void btnPay_Click(object sender, EventArgs e)
+        {
+            //Get Config Info
+            string vnp_Returnurl = ConfigurationManager.AppSettings["vnp_Returnurl"]; //URL nhan ket qua tra ve 
+            string vnp_Url = ConfigurationManager.AppSettings["vnp_Url"]; //URL thanh toan cua VNPAY 
+            string vnp_TmnCode = ConfigurationManager.AppSettings["vnp_TmnCode"]; //Ma định danh merchant kết nối (Terminal Id)
+            string vnp_HashSecret = ConfigurationManager.AppSettings["vnp_HashSecret"]; //Secret Key
+
+            //Get payment input
+            OrderInfo order = new OrderInfo();
+            order.OrderId = DateTime.Now.Ticks; // Giả lập mã giao dịch hệ thống merchant gửi sang VNPAY
+            order.Amount =  long.Parse(amount); // Giả lập số tiền thanh toán hệ thống merchant gửi sang VNPAY 100,000 VND
+            order.Status = "0"; //0: Trạng thái thanh toán "chờ thanh toán" hoặc "Pending" khởi tạo giao dịch chưa có IPN
+            order.CreatedDate = DateTime.Now;
+            order.adId= adId;
+            //Save order to db
+
+            //Build URL for VNPAY
+            VnPayLibrary vnpay = new VnPayLibrary();
+
+            vnpay.AddRequestData("vnp_Version", VnPayLibrary.VERSION);
+            vnpay.AddRequestData("vnp_Command", "pay");
+            vnpay.AddRequestData("vnp_TmnCode", vnp_TmnCode);
+            vnpay.AddRequestData("vnp_Amount", (order.Amount * 100).ToString());
+            if (bankcode_Vnpayqr.Checked == true)
+            {
+                vnpay.AddRequestData("vnp_BankCode", "VNPAYQR");
+            }
+            else if (bankcode_Vnbank.Checked == true)
+            {
+                vnpay.AddRequestData("vnp_BankCode", "VNBANK");
+            }
+            else if (bankcode_Intcard.Checked == true)
+            {
+                vnpay.AddRequestData("vnp_BankCode", "INTCARD");
+            }
+
+            vnpay.AddRequestData("vnp_CreateDate", order.CreatedDate.ToString("yyyyMMddHHmmss"));
+            vnpay.AddRequestData("vnp_CurrCode", "VND");
+            vnpay.AddRequestData("vnp_IpAddr", Utils.GetIpAddress());
+
+            if (locale_Vn.Checked == true)
+            {
+                vnpay.AddRequestData("vnp_Locale", "vn");
+            }
+            else if (locale_En.Checked == true)
+            {
+                vnpay.AddRequestData("vnp_Locale", "en");
+            }
+            vnpay.AddRequestData("vnp_OrderInfo", "Thanh toan don hang:" + order.OrderId);
+            vnpay.AddRequestData("vnp_OrderType", "other"); //default value: other
+
+            vnpay.AddRequestData("vnp_ReturnUrl", vnp_Returnurl);
+            vnpay.AddRequestData("vnp_TxnRef", order.OrderId.ToString()); // Mã tham chiếu của giao dịch tại hệ thống của merchant. Mã này là duy nhất dùng để phân biệt các đơn hàng gửi sang VNPAY. Không được trùng lặp trong ngày
+
+            //Add Params of 2.1.0 Version
+            //Billing
+
+            string paymentUrl = vnpay.CreateRequestUrl(vnp_Url, vnp_HashSecret);
+            log.InfoFormat("VNPAY URL: {0}", paymentUrl);
+            Response.Redirect(paymentUrl);
+        }
+
+
+    }
+}
---
+++ vnpay_cs/VNPAY_CS_ASPX/vnpay_return.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="vnpay_return.aspx.cs" Inherits="VNPAY_CS_ASPX.vnpay_return" Async="true" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>RETURN URL FROM VNPAY</title>
+    <link href="Styles/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+        <div class="header clearfix">
+            <h3 class="text-muted">Kết quả thanh toán</h3>
+        </div>
+        <div class="table-responsive">
+            <div runat="server" id="displayMsg"></div>
+        </div> 
+        <div class="table-responsive">
+            <div runat="server" id="displayTmnCode"></div>
+        </div>
+        <div class="table-responsive">
+            <div runat="server" id="displayTxnRef"></div>
+        </div> 
+        <div class="table-responsive">
+            <div runat="server" id="displayVnpayTranNo"></div>
+        </div> 
+        <div class="table-responsive">
+            <div runat="server" id="displayAmount"></div>
+        </div>
+        <div class="table-responsive">
+            <div runat="server" id="displayBankCode"></div>
+        </div>
+        <div id="countdownTimer"></div>
+    </div>
+    
+    <script type="text/javascript">
+        var secondsLeft = 10;
+
+        function countdown() {
+            if (secondsLeft === 0) {
+                // Redirect to the desired URL after countdown
+                var redirectUrl = "http://localhost:3000" ;
+                window.location.href = redirectUrl;
+            } else {
+                document.getElementById("countdownTimer").innerHTML = "Đang chuyển trang sau " + secondsLeft + " giây...";
+                countdownTimer.style.color = "red";
+                secondsLeft--;
+                setTimeout(countdown, 1000);
+            }
+        }
+
+        countdown();
+    </script>
+</body>
+</html>
